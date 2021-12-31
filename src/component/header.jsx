c2b9e470cf544009d08a6c91c1800e7a2c13ad80
Hao.Tu <hao.tu@alta.com.vn>
2021-12-31 16:18:35 +0700

    add style for header and card

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -11,26 +11,15 @@ import {
 
 const Header = () => {
   return (
-    <Layout.Header className="header">
-      Header
-      <div className="site-card-wrapper">
+    <Layout.Header className="header" style={{ display: "flex" }}>
+      <div className="site-card-wrapper123" style={{ width: "100%", height: "100%" }}>
         <Row gutter={16}>
           <Col span={8}>
             <Card title="Card title" bordered={false}>
               Card content
-              <XYPlot width={300} height={300}>
-                <HorizontalGridLines />
-                <LineSeries
-                  data={[
-                    { x: 1, y: 10 },
-                    { x: 2, y: 5 },
-                    { x: 3, y: 15 },
-                  ]}
-                />
-                <XAxis />
-                <YAxis />
-              </XYPlot>
+
             </Card>
+
           </Col>
           <Col span={8}>
             <Card title="Card title" bordered={false}>
